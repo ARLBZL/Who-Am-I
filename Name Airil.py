178e8e65e75aa87b33dc424dd90e5725eaae1d82
ARLBZL <74575332+ARLBZL@users.noreply.github.com>
2024-06-24 15:17:25 +0800

    Added files birth date and main file

--- Name Airil.py
+++ Name Airil.py
@@ -1,7 +1,25 @@
-name = input("Enter name: ")
-
-if name == "Airil":
+#Check user name
+username = input("Enter name: ")
+name = "Airil"
+if username.lower() == name.lower():
     print("Welcome, Airil")
+    # Python3 code to calculate age in years
+    age = int(input("Enter age: "))
+
+    from datetime import date
+
+    def calculateAge(birthDate):
+        today = date.today()
+        age = today.year - birthDate.year -((today.month, today.day) <
+            (birthDate.month, birthDate.day))
+
+        return age
+        
+    # Driver code 
+    if age == calculateAge(date(2002, 1, 21)):
+        print("Correct.")
+    else:
+        print("Wrong.")
 else:
     print("Access denied")
 
